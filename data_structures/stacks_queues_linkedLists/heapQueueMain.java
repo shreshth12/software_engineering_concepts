38b9b1b409d295e1afead4e2ae89454a29811b5f
Shreshth Kaushik <shreshthsoccer12@gmail.com>
2025-03-01 18:32:01 -0500

    feat(data_structures): Implemented `heapify()` funtionality to `minHeap.java` data structure

--- data_structures/stacks_queues_linkedLists/heapQueueMain.java
+++ data_structures/stacks_queues_linkedLists/heapQueueMain.java
@@ -1,5 +1,8 @@
 package data_structures.stacks_queues_linkedLists;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class heapQueueMain {
     public static void main(String[] args) {
         minHeap heap = new minHeap();
@@ -8,7 +11,7 @@ public class heapQueueMain {
         heap.insert(3);
         heap.insert(7);
         heap.insert(1);
-        System.out.println(heap);
+        System.out.println(heap); // heap/Pq: [1, 3, 5, 20, 7]
 
         /*
          *   heap/Pq: [1, 3, 5, 20, 7]
@@ -21,7 +24,7 @@ public class heapQueueMain {
          */
 
         heap.pop(); // Returns 1, as its the samllest number/highest priority
-        System.out.println(heap);
+        System.out.println(heap); // heap/Pq: [3, 7, 5, 20]
 
         /*
          *   heap/Pq: [3, 7, 5, 20]
@@ -33,5 +36,32 @@ public class heapQueueMain {
          *       20     
          */
         
+         // Create a new array and then convert it into a heap
+         ArrayList<Integer> newArray = new ArrayList<>(Arrays.asList(5, 7, 1, 2, 3)); // heap/Pq: [5, 7, 1, 2, 3]
+
+        /*
+         * Before the actual heapify, the structure formed looks like this
+         * heap/Pq: [5, 7, 1, 2, 3]
+         * 
+         *             5
+         *           /   \
+         *         7      1
+         *       /  \    
+         *      2    3 
+        */
+
+        heap.heapify(newArray);
+
+        /*
+         * After the actual heapify, the structure formed changed to this
+         * heap/Pq: [1, 2, 5, 7, 3]
+         * 
+         *             1
+         *           /   \
+         *         2      5
+         *       /  \    
+         *      7    3 
+        */
+        System.out.println(heap); // heap/Pq: [1, 2, 5, 7, 3]
     }    
 }
--- data_structures/stacks_queues_linkedLists/minHeap.java
+++ data_structures/stacks_queues_linkedLists/minHeap.java
@@ -54,12 +54,12 @@ public class minHeap {
         this.heap.add(value);
 
         // Bubble-up until the "order" property is not valid
-        int curr_idx = this.heap.size() - 1;
-        while(curr_idx > 0 && (this.heap.get(curr_idx) < this.heap.get((curr_idx - 1) / 2))){
-            int root = this.heap.get((curr_idx - 1) / 2);
-            this.heap.set(((curr_idx - 1) / 2), this.heap.get(curr_idx));
-            this.heap.set(curr_idx, root);
-            curr_idx = (curr_idx - 1) / 2;
+        int currIndex = this.heap.size() - 1;
+        while(currIndex > 0 && (this.heap.get(currIndex) < this.heap.get((currIndex - 1) / 2))){
+            int root = this.heap.get((currIndex - 1) / 2);
+            this.heap.set(((currIndex - 1) / 2), this.heap.get(currIndex));
+            this.heap.set(currIndex, root);
+            currIndex = (currIndex - 1) / 2;
         }
     }
 
@@ -72,7 +72,7 @@ public class minHeap {
      */
     public int pop(){
         // Check if the heap is not empty
-        if(heap.isEmpty()){
+        if(this.heap.isEmpty()){
            return -1; 
         }
 
@@ -90,32 +90,32 @@ public class minHeap {
         this.heap.remove(this.heap.size() - 1);
 
         // Bubble the current highestPriorityItem until the "order" is correct
-        int curr_idx = 0;
-        while((2 * curr_idx) + 1 < this.heap.size()){
-            int leftChildIndex = (2 * curr_idx) + 1;
-            int rightChildIndex = (2 * curr_idx) + 2;
+        int currIndex = 0;
+        while((2 * currIndex) + 1 < this.heap.size()){
+            int leftChildIndex = (2 * currIndex) + 1;
+            int rightChildIndex = (2 * currIndex) + 2;
 
             // Case 1: When the rightChild is smaller than the leftChild + currentNode
             if ( 
                 rightChildIndex < this.heap.size() && // Right child exits?
                 this.heap.get(rightChildIndex) < this.heap.get(leftChildIndex) && // Is the rightChild smaller than the leftchild?
-                this.heap.get(curr_idx) > this.heap.get(rightChildIndex) // Is the currChild greater than rightChild? i.e also from both?
+                this.heap.get(currIndex) > this.heap.get(rightChildIndex) // Is the currChild greater than rightChild? i.e also from both?
                )
             {
                 int temp = this.heap.get(rightChildIndex);
-                this.heap.set(rightChildIndex, this.heap.get(curr_idx));
-                this.heap.set(curr_idx, temp);
-                curr_idx = rightChildIndex;
+                this.heap.set(rightChildIndex, this.heap.get(currIndex));
+                this.heap.set(currIndex, temp);
+                currIndex = rightChildIndex;
             }
 
             // Case 2: When the leftChild is smaller than the rightChild + currentNode
             else if (
-                this.heap.get(leftChildIndex) < this.heap.get(curr_idx)
+                this.heap.get(leftChildIndex) < this.heap.get(currIndex)
             ){
                 int temp = this.heap.get(leftChildIndex);
-                this.heap.set(leftChildIndex, this.heap.get(curr_idx));
-                this.heap.set(curr_idx, temp);
-                curr_idx = leftChildIndex;
+                this.heap.set(leftChildIndex, this.heap.get(currIndex));
+                this.heap.set(currIndex, temp);
+                currIndex = leftChildIndex;
             }
 
             // Case 3: Neither is smaller than the currentNode, exit
@@ -125,6 +125,66 @@ public class minHeap {
         }
 
         return highestPriorityItem;
+    }
+
+    /*
+     * This method takes in a single argument: ArrayList<Integer> array
+     * and converts that array into a heap.
+     */
+    public void heapify(ArrayList<Integer> array){
+        // Make the current array as the heap
+        this.heap = array;
+
+         /* 
+          Since the leaf nodes/element satisfy both structure and order
+          property, we only need to bubble down the numbers which are non-leaf
+          That is, we only look at half of the nodes, which is N // 2
+         */
+
+        int currIndex = (this.heap.size() / 2) - 1;
+
+        // TODO: the bubbling down logic is duplicated across the current and pop method.
+        //       create a global function to do this.
+
+        while(currIndex >= 0){
+            int bubbleDownIndex = currIndex;
+
+            // Keep bubbling down until there are no children
+            while((2 * bubbleDownIndex) + 1 < this.heap.size()){
+                int leftChildIndex = (2 * bubbleDownIndex) + 1;
+                int rightChildIndex = (2 * bubbleDownIndex) + 2;
+
+                // Case 1: When the rightChild is smaller than the leftChild + currentNode
+                if ( 
+                    rightChildIndex < this.heap.size() && // Right child exits?
+                    this.heap.get(rightChildIndex) < this.heap.get(leftChildIndex) && // Is the rightChild smaller than the leftchild?
+                    this.heap.get(bubbleDownIndex) > this.heap.get(rightChildIndex) // Is the currChild greater than rightChild? i.e also from both?
+                ){
+                    int temp = this.heap.get(rightChildIndex);
+                    this.heap.set(rightChildIndex, this.heap.get(bubbleDownIndex));
+                    this.heap.set(bubbleDownIndex, temp);
+                    bubbleDownIndex = rightChildIndex;
+                }
+
+                // Case 2: When the leftChild is smaller than the rightChild + currentNode
+                else if (
+                    this.heap.get(leftChildIndex) < this.heap.get(bubbleDownIndex)
+                ){
+                    int temp = this.heap.get(leftChildIndex);
+                    this.heap.set(leftChildIndex, this.heap.get(bubbleDownIndex));
+                    this.heap.set(bubbleDownIndex, temp);
+                    bubbleDownIndex = leftChildIndex;
+                }
+
+                // Case 3: Neither is smaller than the currentNode, exit
+                else{
+                    break;
+                }
+            }
+
+            // Move to the next element to bubble down
+            currIndex--;
+        }
     }
 
     // Method to print out the current heap
